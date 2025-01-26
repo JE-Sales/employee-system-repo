58a6f69503b15d66a16b998dd87ad0b1b220b803
John Emmanuel Sales <salesjohnemmanuel@gmail.com>
2025-01-26 21:43:10 +0800

    UPDATE Users UNTESTED

--- employee-system-backend/src/controllers/users.ts
+++ employee-system-backend/src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUserById, getUsers } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) : Promise<any> => {
     try {
@@ -20,6 +20,27 @@ export const deleteUser = async (req: express.Request, res: express.Response) : 
         const deletedUser = await deleteUserById(id);
         
         return res.json(deletedUser); 
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) : Promise<any> => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if (!username) {
+            return res.sendStatus(400);
+        }
+
+        const user = getUserById(id);
+
+        (await user).username = username;
+        (await user).save;
+
+        return res.status(200).json(user).end;
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
